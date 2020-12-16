266f4797b9632a29f3ce3fbdd3109e014e4adbe0
syauqeesy <syauqeesy@gmail.com>
2020-12-16 22:34:05 +0700

    Verify user done

--- routes/product.js
+++ routes/product.js
@@ -1,6 +1,7 @@
 const product = require('../controllers/product')
+const verifyUser = require('../middlewares/verifyUser')
 const router = require('express').Router()
 
 module.exports = router
-  .get('/', product.getAllProducts)
-  .post('/create', product.createProduct)
+  .get('/', verifyUser, product.getAllProducts)
+  .post('/create', verifyUser, product.createProduct)
