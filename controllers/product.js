f15376bb7d1f615a3cde15ee6d8d229188564caf
syauqeesy <syauqeesy@gmail.com>
2020-12-17 16:57:42 +0700

    Get all products done

--- controllers/product.js
+++ controllers/product.js
@@ -2,8 +2,46 @@ const { v4: uuid } = require('uuid')
 const productModel = require('../models/product')
 
 module.exports = {
-  getAllProducts: (req, res) => {
-    res.send(__dirname)
+  getAllProducts: async (req, res) => {
+    try {
+      const page = parseInt(req.query.page) || 1
+      const result = await productModel.getAllProducts({
+        keyword: req.query.keyword || '',
+        sort: req.query.sort || 'ASC',
+        page: page
+      })
+
+      if(result.length < 1) return res.status(404).send({
+        status: 'Failed',
+        statusCode: 404,
+        message: 'No product found!'
+      })
+
+      const numOfProducts = await productModel.getNumOfProducts({
+        keyword: req.query.keyword || '',
+        sort: req.query.sort || 'ASC'
+      })
+
+      const pagination = {
+        previous: page - 1 > 0 ? page - 1 : null,
+        current: page,
+        next: page + 1 <= Math.ceil(numOfProducts.length / 12) ? page + 1 : null
+      }
+
+      return res.status(200).send({
+        status: 'Success',
+        statusCode: 200,
+        products: result,
+        pagination: pagination
+      })
+    } catch (error) {
+      console.log(error)
+      return res.status(500).send({
+        status: 'Failed',
+        statusCode: 500,
+        message: 'Internal server error!'
+      })
+    }
   },
   createProduct: async (req, res) => {
     try {
--- models/product.js
+++ models/product.js
@@ -1,6 +1,25 @@
 const db = require('./database')
+require('dotenv').config()
 
 module.exports = {
+  getNumOfProducts: (data) => {
+    return db(`SELECT * FROM products WHERE name LIKE '%${data.keyword}%' ORDER BY price ${data.sort}`)
+    .then(result => {
+      return result
+    })
+    .catch(error => {
+      throw error
+    }) 
+  },
+  getAllProducts: (data) => {
+    return db(`SELECT id, name, CONCAT('${process.env.BASE_URL}/images/', image) as image, price FROM products WHERE name LIKE '%${data.keyword}%' ORDER BY price ${data.sort} LIMIT ${data.page * 12 - 12}, 12`)
+    .then(result => {
+      return result
+    })
+    .catch(error => {
+      throw error
+    }) 
+  },
   insertProduct: (data) => {
     return db(`INSERT INTO products SET id = '${data.id}', name = '${data.name}', description = '${data.description}', image = '${data.image}', stock = ${data.stock}, category = '${data.category}', price = ${data.price}`)
     .then(result => {
